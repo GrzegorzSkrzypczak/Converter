8f2bd63ec8ee61311b0cd434539804a7be23bdf0
Grzegorz <rowingteam.grzegorz@gmail.com>
2020-05-19 19:01:30 +0200

    Z cobry

--- src/ProjectApka/Main.java
+++ src/ProjectApka/Main.java
@@ -3,6 +3,6 @@ package ProjectApka;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hello Grześ");
+        System.out.println("hello Grześ, Cobra");
     }
 }
