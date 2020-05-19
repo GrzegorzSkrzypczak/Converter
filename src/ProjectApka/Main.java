94a4c37ab690e972e65a58985c893eb33ec52c81
Grzegorz <rowingteam.grzegorz@gmail.com>
2020-05-19 19:10:39 +0200

    dzieje sie

--- src/ProjectApka/Main.java
+++ src/ProjectApka/Main.java
@@ -7,6 +7,7 @@ public class Main {
         System.out.println("hello Rafciu");
         System.out.println("hello Grześ, Cobra");
         System.out.println("co slychac?");
+        System.out.println("nic ciekawego, syf i dolina :D");
 
     }
 }
