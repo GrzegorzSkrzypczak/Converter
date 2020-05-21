2e10f572af7b752ef524842d0720da828e054724
Grzegorz <rowingteam.grzegorz@gmail.com>
2020-05-21 21:17:08 +0200

    Według Łafała zepsułem klasę Mass

--- src/ProjectApka/Main.java
+++ src/ProjectApka/Main.java
@@ -3,6 +3,8 @@ package ProjectApka;
 public class Main {
 
     public static void main(String[] args) {
+
+        System.out.println("Temperature: ");
         Temperature temp = new Temperature();
         System.out.println(temp.celsiusToKelwin(10));
         System.out.println(temp.celsiusToFahrenheit(10));
@@ -10,6 +12,18 @@ public class Main {
         System.out.println(temp.kelwinToCelsius(283.15));
         System.out.println(temp.fahrenheitToCelsius(50));
         System.out.println(temp.fahrenheitToKelwin(-441.70));
+
+        System.out.println();
+        System.out.println("Mass: ");
+        System.out.println("____________________________________");
+
+        Mass mass = new Mass();
+        mass.kilogramToPunds(1);
+        mass.kilogramToUncja(1);
+        mass.poundsToKilograms(1);
+        mass.poundsToUncja(1);
+        mass.uncjaToKilograms(1);
+        mass.uncjaToPonds(1);
 
 
     }
--- src/ProjectApka/Mass.java
+++ src/ProjectApka/Mass.java
@@ -6,4 +6,35 @@ public class Mass {
     double pounds;
     double uncja;
 
+    void kilogramToPunds(double kilograms) {
+        this.kg = kilograms;
+        System.out.format("%.2f%n",kilograms * 2.2046);
+    }
+
+    void kilogramToUncja(double kilograms) {
+        this.kg = kilograms;
+        System.out.format("%.2f%n",kilograms * 35.274);
+    }
+
+    void poundsToKilograms(double pounds) {
+        this.pounds = pounds;
+        System.out.format("%.2f%n",pounds * 0.45359237);
+    }
+
+    void poundsToUncja(double pounds) {
+        this.pounds = pounds;
+        System.out.format("%.2f%n",pounds * 16);
+    }
+
+    void uncjaToKilograms(double uncja) {
+        this.uncja = uncja;
+        System.out.format("%.2f%n",uncja / 35.274);
+    }
+
+    void uncjaToPonds(double uncja) {
+        this.uncja = uncja;
+        System.out.format("%.2f%n",uncja * 0.062500);
+    }
+
+
 }
