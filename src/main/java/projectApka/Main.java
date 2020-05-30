86102e4f4a165d776d5e6f1ccc564007dc4abd56
Grzegorz <rowingteam.grzegorz@gmail.com>
2020-05-30 13:17:32 +0200

    usuniety main

--- src/main/java/projectApka/Main.java
+++
@@ -1,8 +0,0 @@
-import dataViewController.JavaFXViewController;
-
-public class Main {
-    public static void main(String[] args) {
-        JavaFXViewController okienko = new JavaFXViewController ();
-        okienko.run ();
-    }
-}
