3c5a537d1d8e04aade9ab7726039804ce9ebe76d
Grzegorz <rowingteam.grzegorz@gmail.com>
2020-05-30 13:19:01 +0200

    Cos sie krzaczy

---
+++ src/main/java/ProjectApka/Main.java
@@ -0,0 +1,9 @@
+import dataViewController.JavaFXViewController;
+
+public class Main {
+    public static void main(String[] args) {
+        JavaFXViewController cont = new JavaFXViewController();
+        cont.run();
+
+    }
+}
